a9c47e70322d57e1d0df7e4ffd21577c60ac9f4b
erikngigi <ericmosesngigi@gmail.com>
2025-10-14 14:09:52 +0300

    fix: remove deprecated variable from root main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,6 @@ module "security" {
   source                      = "./modules/security"
   project_name                = var.project_name
   git_oidc_url                = var.git_oidc_url
-  oidc_thumbprint             = var.oidc_thumbprint
   github_org                  = var.github_org
   github_repo                 = var.github_repo
   git_oidc_thumbprint_list    = var.git_oidc_thumbprint_list
